9c6193e3fd9a2130c55dff89f3d463af0968dd41
Yoraver <singhvirkyoraver@gmail.com>
2024-06-22 14:09:32 +0530

    Make add tocart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,9 +44,45 @@ products.forEach((product) => {
           <div class="added-to-cart">
             <img src="images/icons/checkmark.png">
             Added
-          </div>`;
+          </div>
+          
+          <button class="add-to-cart-button
+            button-primary js-add-to-cart"
+            data-product-id="${product.id}">
+            Add to Cart
+          </button>
+          </div>  
+          `;
         });
 
         console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;        
+document.querySelector('.js-products-grid').innerHTML = productsHTML; 
+
+document.querySelectorAll('.js-add-to-cart')
+ .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.
+      productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId){
+            matchingItem = item;
+        }
+      });  
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else { 
+      cart.push({
+        productId: productId,
+        quantity:1
+      });
+    }
+
+      
+      console.log(cart);  
+    });
+ });
