619a72093a502ce805e7955a6c4b8de023fc31b4
Yoraver <singhvirkyoraver@gmail.com>
2024-06-25 23:04:19 +0530

    Move orderSummary into its own file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -11,6 +11,7 @@ const today = dayjs();
 const deliveryDate = today.add(7, 'days');
 console.log(deliveryDate.format('dddd, MMMM D'));
 
+function renderOrderSummary() {
 let cartSummaryHTML ='';
 
 cart.forEach((cartItem) => {
@@ -147,5 +148,9 @@ document.querySelectorAll('.js-delivery-option')
     element.addEventListener('click', () => {
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummary();
     });
-  });    
+  });  
+}
+
+renderOrderSummary();
