1ae03c3e56e286e6cf2a285c2cec1774a58b08c3
Yoraver <singhvirkyoraver@gmail.com>
2024-06-26 12:29:09 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import {formatCurrency} from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 let cartSummaryHTML ='';
@@ -128,6 +123,8 @@ document.querySelectorAll('.js-delete-link')
             const container = document.querySelector
                 (`.js-cart-item-container-${productId}`);
             container.remove();    
+
+            renderPaymentSummary();
         });
     });    
 
@@ -137,6 +134,7 @@ document.querySelectorAll('.js-delivery-option')
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });  
 }
